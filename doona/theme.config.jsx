d330bb22ed7e38022dcea4c987bbc7743da232d2
Kyle Awayan <kyle@awayan.com>
2023-11-16 18:53:05 -0800

    edit footer

--- doona/theme.config.jsx
+++ doona/theme.config.jsx
@@ -8,13 +8,7 @@ export default {
   dateFormatter: date => `Last updated at ${date.toDateString()}`,
   footer: (
     <small style={{ display: 'block', marginTop: '8rem' }}>
-      <abbr
-        title="This site and all its content are licensed under a Creative Commons Attribution-NonCommercial 4.0 International License."
-        style={{ cursor: 'help' }}
-      >
-        CC BY-NC 4.0
-      </abbr>{' '}
-      {new Date().getFullYear()} © Shu Ding.
+      {new Date().getFullYear()} © Kyle Awayan.
       <a href="/feed.xml">RSS</a>
       <style jsx>{`
         a {
