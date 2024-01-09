5a82ba714a529c4c2729d49d3f134b7aa56b8434
Kyle Awayan <kyle@awayan.com>
2024-01-09 10:16:07 -0800

    build resume script

---
+++ doona/build-resume.js
@@ -0,0 +1,22 @@
+import puppeteer from 'puppeteer'
+
+const resumeUrl = 'http://localhost:3000/resume'
+
+;(async () => {
+  const browser = await puppeteer.launch()
+  const page = await browser.newPage()
+  await page.goto(resumeUrl, { waitUntil: 'networkidle2' })
+  await page.pdf({
+    path: './public/Kyle Awayan Resume.pdf',
+    format: 'letter',
+    margin: {
+      top: '1cm',
+      right: '1cm',
+      bottom: '1cm',
+      left: '1cm'
+    },
+    tagged: true
+  })
+
+  await browser.close()
+})()
